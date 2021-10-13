7d05d72cab215f62f0de2fec846e31f10374295d
David Asgar-Deen <asgardee@ualberta.ca>
2021-10-13 12:40:53 -0600

    Unity script

---
+++ NDI 5 DoF Tracking/receiver.m
@@ -0,0 +1,30 @@
+%% Preapare UDP connections 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% UDPs for sensor reading %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+hudpr = dsp.UDPReceiver;
+hudps = dsp.UDPSender;
+hudpr.LocalIPPort  = 1501;
+
+hudps.RemoteIPPort = 1500;
+hudps.LocalIPPort  = 1501;
+hudps.RemoteIPAddress = '127.0.0.1';
+
+
+DATA =zeros(3,10);
+release(hudpr)
+fprintf('Start to receive ... ');
+    for j=1:1:10
+        dataReceived =hudpr();
+        while(isempty(dataReceived))
+            dataReceived = step(hudpr);
+            pause(0.1);
+        end
+        
+        y = typecast(dataReceived, 'double');
+        pause(0.1);
+        
+        DATA(:,j)=y;
+        display(y);
+    end
