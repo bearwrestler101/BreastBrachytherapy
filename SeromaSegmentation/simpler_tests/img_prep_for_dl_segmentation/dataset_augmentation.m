148b0172141694bf43004e8c3db4b1dfd8e5d987
bearwrestler101 <makhach@ualberta.ca>
2022-08-18 15:20:59 -0600

    Backup

--- SeromaSegmentation/simpler_tests/img_prep_for_dl_segmentation/dataset_augmentation.m
+++ SeromaSegmentation/simpler_tests/img_prep_for_dl_segmentation/dataset_augmentation.m
@@ -5,12 +5,51 @@ for i=1:length(origImglist)
   origImg{i}=imread("dataset/"+origImglist(i).name);
 end
 
+%% Resize to 256x256
+imsize = 256;
+resizedImg = cell(size(origImg,1), size(origImg,2));
+
+for i = 1:length(origImg)
+    resizedImg{i}=imresize(origImg{i}, [imsize imsize]);
+end
+
 %%
 
-[gammaUp, gammaDown] = deal(origImg);
+for k = 1:size(resizedImg, 2)
+    if isa(resizedImg{k}, 'uint8')
+
+        snpNoise = zeros(round(imsize*1), round(imsize*0.3));
+        rng('shuffle');
+
+        %random points where zeros turned to ones
+        row = randi([1 size(snpNoise,1)],1,200);
+        column = randi([1 size(snpNoise,2)],1,200);
+
+        for i=1:size(row,2)
+            snpNoise(row(i),column(i)) = 1;
+            
+            %adding extra ones around a point to create clusters
+            if row(i)>4 && row(i) <=size(snpNoise,1)-4 && column(i)>4 && column(i)<=size(snpNoise,2)-4 && rand > 0.6
+                rowExtra = randi([-3 3], [4 1]);
+                columnExtra = randi([-3 3], [4 1]);
+                for j = 1:size(rowExtra,1)
+                    snpNoise(row(i)+rowExtra(j),column(i)+columnExtra(j)) = 1;
+                end
+            end
+        end
+        snpNoise = imresize(snpNoise,[imsize, imsize]);
+        snpNoise = uint8(255 * mat2gray(snpNoise,[0 1]));
+        snpNoise = imgaussfilt(snpNoise,1);
+        resizedImg{k} = imadd(snpNoise, resizedImg{k}(:,:,1));
+    end
+end
+
+%% Gamma Up, Down and Rotate 90, 180, 270
+
+[gammaUp, gammaDown] = deal(resizedImg);
 
 for i = 1:length(gammaUp)
-    if isa(gammaUp{i}, "uint8")
+    if isa(gammaUp{i}, "uint8") %imgs=uint8, masks=logical
         gammaUp{i} = imadjust(gammaUp{i},[],[],0.7);
         gammaDown{i} = imadjust(gammaDown{i},[],[],1.5);
         continue
@@ -20,7 +59,7 @@ end
 [rot90_gUp, rot180_gUp, rot270_gUp] = deal(gammaUp);
 [rot90_gDown, rot180_gDown, rot270_gDown] = deal(gammaDown);
 
-for i = 1:length(origImg)
+for i = 1:length(resizedImg)
     rot90_gUp{i} = imrotate(rot90_gUp{i}, 90);
     rot180_gUp{i} = imrotate(rot180_gUp{i}, 180);
     rot270_gUp{i} = imrotate(rot270_gUp{i}, 270);
@@ -29,8 +68,8 @@ for i = 1:length(origImg)
     rot270_gDown{i} = imrotate(rot270_gDown{i}, 270);
 end
 
-%%
-dataset = [origImg, gammaUp, gammaDown, rot90_gUp, rot90_gDown, rot180_gUp, rot180_gDown, rot270_gUp, rot270_gDown];
+%% Combine all versions
+dataset = [resizedImg, gammaUp, gammaDown, rot90_gUp, rot90_gDown, rot180_gUp, rot180_gDown, rot270_gUp, rot270_gDown];
 
 %%
 [imgs, masks, masks1, masks2] = deal([]);
@@ -55,7 +94,8 @@ for i = 1:9 %upper limit of foor loop specifies how many variations of the origi
     for j = 1:length(imgs)
         k = imgs(j) + (i-1)*length(origImglist); %finds only images in dataset using imgs list
         imgNum = j + (i-1)*length(imgs); %imgNum allows naming the images in a consecutive order
-        imwrite(dataset{k}, "augmented_dataset/benign (" + imgNum + ").png")
+        dataset{k} = cat(3, dataset{k}, dataset{k}, dataset{k});
+        imwrite(dataset{k}, "snp_augmented_dataset/benign (" + imgNum + ").png")
     end
 end
 
@@ -65,12 +105,12 @@ for i = 1:9
     for j = 1:length(masks)
         k = masks(j) + (i-1)*length(origImglist);
         maskNum = j + (i-1)*length(masks);
-        imwrite(dataset{k}, "augmented_dataset/benign (" + maskNum + ")_mask.png");
+        imwrite(dataset{k}, "snp_augmented_dataset/benign (" + maskNum + ")_mask.png");
         if find(masks1== k-(i-1)*length(origImglist)+1)
-            imwrite(dataset{k+1}, "augmented_dataset/benign (" + maskNum + ")_mask_1.png");
+            imwrite(dataset{k+1}, "snp_augmented_dataset/benign (" + maskNum + ")_mask_1.png");
         end
         if find(masks2 == k-(i-1)*length(origImglist)+2)
-            imwrite(dataset{k+2}, "augmented_dataset/benign (" + maskNum + ")_mask_2.png");
+            imwrite(dataset{k+2}, "snp_augmented_dataset/benign (" + maskNum + ")_mask_2.png");
         end
 
     end
