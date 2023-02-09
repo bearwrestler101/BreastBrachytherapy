ce0ec66c8b5691475f6cc29573257bbb58dfb4a1
Kirill Makhachev <makhach@ualberta.ca>
2023-02-08 19:04:59 -0700

    Added new reconstructed seroma mesh - exported as .obj from matlab; generated its backfaces by duplicating mesh and flipping normals

---
+++ NDI 5 DoF Tracking/ARDisplay - Breast Brachytherapy/Assets/Scripts/Flip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+public class Flip : MonoBehaviour
+{
+    [ContextMenu("Flip")]
+    public void FlipMeshContext()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
