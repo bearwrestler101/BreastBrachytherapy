d0233c327c0696d427db87974cae323fb73d06cc
Kirill Makhachev <makhach@ualberta.ca>
2023-03-08 17:02:31 -0700

    Implemented camera views, registered everything to panda base, added .csv logging

--- NDI 5 DoF Tracking/ARDisplay - Breast Brachytherapy/Assets/Scripts/Flip.cs
+++ NDI 5 DoF Tracking/ARDisplay - Breast Brachytherapy/Assets/Scripts/Flip.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿// source: https://answers.unity.com/questions/476810/flip-a-mesh-inside-out.html
+// helpful for correct syntax: https://docs.unity3d.com/ScriptReference/ContextMenu.html
+
+// context menu is at gear of the script component attached to the object
+
+
+using UnityEngine;
 using System.Linq;
 using System.Collections;
 
