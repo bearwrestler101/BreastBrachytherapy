267764098d72ffd42d72c1f02ce835c4c2057b50
bearwrestler101 <makhach@ualberta.ca>
2023-06-28 14:52:26 -0600

    Removed some folders that forgot to include in gitignore

--- SeromaSegmentation/Segmentation/our_dataset/ROI_Mask/roi.m
+++
@@ -1,9 +0,0 @@
-combined_cell = [cell'; cell2'; cell3'; cell4'; cell5'; cell6'];
-
-
-    for i = 1:height(combined_cell) 
-        imshow(combined_cell{i})
-        ROI = drawassisted;
-        mask = createMask(ROI);
-        imwrite(mask, i+".png")
-    end 
--- SeromaSegmentation/Segmentation/our_dataset/ROI_Mask_Good/roi.m
+++
@@ -1,9 +0,0 @@
-combined_cell_good = [FRH100M_F1_D45_G85_good; FRH100M_F1_D45_G90_good; FRH60M_F1_D45_G80_good];
-
-
-    for i = 1:height(combined_cell_good) 
-        imshow(combined_cell_good{i})
-        ROI = drawassisted;
-        mask = createMask(ROI);
-        imwrite(mask, i+".png")
-    end 
